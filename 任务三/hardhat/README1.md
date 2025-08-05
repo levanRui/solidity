cf43f3b513b75b692db38113cc10ee3aa822f7a4
levanRui <1178912550@qq.com>
2025-08-05 17:22:51 +0800

    任务三 NFT拍卖合约

---
+++ 任务三/hardhat/README1.md
@@ -0,0 +1,33 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat ignition deploy ./ignition/modules/Lock.js
+```
+1.安装node.js和npm设置   
+管理员模式：Set-ExecutionPolicy RemoteSigned   npm init -y
+2.npm install --save-dev hardhat
+3.npx hardhat init
+4.npx hardhat node
+5.npx hardhat ignition deploy ./ignition/modules/Lock.js --network localhost  //部署到本地
+npx hardhat ignition deploy ./ignition\modules\Lock.js --network sepolia   //部署到sepolia测试环境
+6.npm install dotenv
+重复部署问题：reset
+7.npx hardhat ignition deploy ./ignition/modules/Lock.js --network sepolia  --reset
+
+
+
+// 拍卖流程
+1.拍卖创建
+2.竞拍开始
+3.竞拍进行
+4.竞拍结束
+5.支付结算
+6.交付拍品
---
+++ 任务三/hardhat/contracts/MyNFT.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+// 导入OpenZeppelin的ERC721实现和Ownable用于权限管理
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract MyNFT is ERC721, Ownable {
+    uint256 public nftTokenCounter;
+    // 构造函数：初始化NFT名称和符号，设置部署者为所有者
+    constructor(
+        string memory name, 
+        string memory symbol,
+        address initialOwner
+    ) ERC721(name, symbol) Ownable(initialOwner) {
+        nftTokenCounter = 1; 
+        initialOwner = msg.sender; // 设置初始所有者为部署者
+    }
+    // 铸造NFT
+    function mintNFT(address toAddress) public onlyOwner returns (uint256) {
+        uint256 newTokenId = nftTokenCounter;
+        _safeMint(toAddress, newTokenId);
+        nftTokenCounter++;
+        return newTokenId;
+    }
+
+        /**
+     * 转移NFT
+     * 任何NFT持有者都可以调用此函数转移自己拥有的NFT
+     * @param from 发送者地址
+     * @param to 接收者地址
+     * @param tokenId 要转移的NFT ID
+     */
+    function transferNFT(address from, address to, uint256 tokenId) public {
+        // 检查调用者是否为所有者或被授权者
+        require(isApprovedForAll(_msgSender(),ownerOf(tokenId)), "caller is not owner nor approved");
+        _transfer(from, to, tokenId);
+    }
+    
+    /**
+     * @dev 获取下一个将要铸造的NFT ID
+     */
+    function getNextTokenId() public view returns (uint256) {
+        return nftTokenCounter;
+    }
+
+}
